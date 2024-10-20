854e1894586d174ab03da6af40e665dfa23dfe63
holidayhome <66779182+GitGorman@users.noreply.github.com>
2024-10-20 18:21:17 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,91 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { HoverPopover, MarkdownRenderer, Plugin, TFile, moment} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class ActivityTracker extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.app.workspace.onLayoutReady( async () => {
+			//find the file that contains the data
+			let files = this.app.vault.getMarkdownFiles();
+			let mondayFileName = moment().startOf('isoWeek').format("YYYY-MM-DD").toString()+".md";
+			let mondayFile = files.find((x: { name: string; }) => x.name == mondayFileName);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			if (mondayFile) {
+				await this.createActivity("run_value","👟", "runbutton" , 6 , mondayFile);
+				await this.createActivity("cycle_value","🚲", "cyclebutton" , 6 , mondayFile);
+				await this.createActivity("yoga_value","🧘", "yogabutton" , 7 , mondayFile);
+				await this.createActivity("workout_value","💪", "workoutbutton" , 5 , mondayFile);
+				await this.createActivity("study_value","🎓", "studybutton" , 30 , mondayFile);
+				await this.createActivity("project_value","🎮", "projectbutton" , 10 , mondayFile);
+				await this.createActivity("maintenance_value","🧹", "maintenancebutton" , 10 , mondayFile);
+				await this.createActivity("hydration_value","💧", "hydrationbutton" , 35 , mondayFile);
+				await this.createActivity("eating_value","🍎", "eatingbutton" , 35 , mondayFile);
+				await this.createActivity("giving_value","🎁", "givingbutton" , 20 , mondayFile);
+				await this.createActivity("knowledge_value","📚", "knowledgebutton" , 7 , mondayFile);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	async createActivity(metadataValue : string, emoji : string, buttonClass : string, maxValue : number, mondayFile : TFile) {
+		//create status bar element
+		let statusBarButton = this.addStatusBarItem().createEl('button');
+		statusBarButton.addClass(buttonClass);			
+		//read all the data from the file
+		var fileData = await this.app.vault.read(mondayFile);
+		let value = "";
+			
+		let generateButtonText = (textValue : string) => {		
+			statusBarButton.setText(emoji);
+			for(let i = 1; i <= maxValue; i = i+1){
+				if (i <= parseInt(textValue)) {
+					statusBarButton.textContent += `▮`;
+				}
+				else {
+					statusBarButton.textContent += "▯";
 				}
 			}
+		}
+
+		let generateButtonTextNoBoxes = () => {		
+			statusBarButton.setText(emoji);		
+		}
+
+		//find the line that contains the desired data
+		var fileDataArray = fileData.split('\n');
+		fileDataArray.forEach(line => {
+			if (line.contains(metadataValue)) {
+				//sepeate the value from the key
+				let keyAndValue = line.split(':');
+				
+				//read from the key and value pair
+				value = keyAndValue[1].toString();
+
+				//write the value to the status bar element
+				generateButtonTextNoBoxes();
+			}
 		});
+						
+		statusBarButton.addEventListener('click', () => {	
+			if (statusBarButton.hasClass("active")) {
+				generateButtonTextNoBoxes();
+				statusBarButton.removeClass("active")
+			}
+			else {
+				generateButtonText(value);
+				statusBarButton.addClass("active")
+			}
+		});		
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		statusBarButton.addEventListener('contextmenu', () => {	
+			if (!statusBarButton.hasClass("active"))
+				return;
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			if (parseInt(value) < maxValue) {
+				value = `${parseInt(value) + 1}`;
+				generateButtonText(value);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				this.app.vault.process(mondayFile, (fileData) => {
+					return fileData.replace(`${metadataValue}: ${parseInt(value)-1}`, `${metadataValue}: ${parseInt(value)}`);
+				});
+			}
+		});						
 	}
 }
